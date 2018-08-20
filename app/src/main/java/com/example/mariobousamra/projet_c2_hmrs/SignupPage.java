f41772135b327feea64ce1c5701494a36469bab4
HazemHalawi <Hazem.halawi@isae.edu.lb>
2018-08-20 17:24:32 +0300

    Fixed Bug

--- app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/SignupPage.java
+++ app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/SignupPage.java
@@ -2,12 +2,36 @@ package com.example.mariobousamra.projet_c2_hmrs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SignupPage extends AppCompatActivity {
+
+
+    private EditText Name;
+    private EditText LastName;
+    private EditText PhoneNumber;
+    private EditText EmailAddress;
+    private EditText Password;
+    private TextView Location;
+    private Button Create;
+    private Button Cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+
+        //referencing
+        Name = (EditText)findViewById(R.id.etName);
+        LastName = (EditText)findViewById(R.id.etLastName);
+        PhoneNumber = (EditText)findViewById(R.id.etPhone);
+        EmailAddress = (EditText)findViewById(R.id.etEmailAddress);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Location = (TextView)findViewById(R.id.tvLocation);
+
+        Create = (Button)findViewById(R.id.btnCreate);
+        Cancel = (Button)findViewById(R.id.btnCancel);
     }
 }
