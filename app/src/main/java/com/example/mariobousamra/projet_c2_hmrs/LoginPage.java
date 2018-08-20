b734a65088305057d9bcfccd2214de2f46d13fda
HazemHalawi <Hazem.halawi@isae.edu.lb>
2018-08-20 17:06:12 +0300

    Fixed Bug

--- app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/LoginPage.java
+++ app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/LoginPage.java
@@ -2,12 +2,34 @@ package com.example.mariobousamra.projet_c2_hmrs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class LoginPage extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView ForgotPassword;
+    private TextView SignUp;
+    private Button Login;
+    private Button BackToMain;
+    private LinearLayout llayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPass);
+        ForgotPassword = (TextView)findViewById(R.id.tvForgotPass);
+
+        Login = (Button)findViewById(R.id.btnLogin);
+        SignUp = (TextView)findViewById(R.id.tvSignUp);
+
+        BackToMain = (Button)findViewById(R.id.btnBackToMain);
     }
 }
