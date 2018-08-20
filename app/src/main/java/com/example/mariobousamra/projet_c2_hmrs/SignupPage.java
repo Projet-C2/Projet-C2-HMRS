74a437dc05374c7013a8aa57444adbd803a166f6
HazemHalawi <Hazem.halawi@isae.edu.lb>
2018-08-20 17:26:32 +0300

    User Validation Feature is added

--- app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/SignupPage.java
+++ app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/SignupPage.java
@@ -2,9 +2,11 @@ package com.example.mariobousamra.projet_c2_hmrs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignupPage extends AppCompatActivity {
 
@@ -33,5 +35,33 @@ public class SignupPage extends AppCompatActivity {
 
         Create = (Button)findViewById(R.id.btnCreate);
         Cancel = (Button)findViewById(R.id.btnCancel);
+
+        Create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(validate()){
+                    //upload data to database
+                }
+            }
+        });
+    }
+
+    private Boolean validate(){
+        boolean result = false;
+
+        String name = Name.getText().toString();
+        String lastname = LastName.getText().toString();
+        String phone = PhoneNumber.getText().toString();
+        String email = EmailAddress.getText().toString();
+        String password = Password.getText().toString();
+        String location = Location.getText().toString();
+
+        if(name.isEmpty() || lastname.isEmpty() || phone.isEmpty() || email.isEmpty() || password.isEmpty() || location.isEmpty()){
+            Toast.makeText(this, "please enter all the details", Toast.LENGTH_SHORT).show();
+        }else{
+            result = true;
+        }
+        return result;
     }
 }
