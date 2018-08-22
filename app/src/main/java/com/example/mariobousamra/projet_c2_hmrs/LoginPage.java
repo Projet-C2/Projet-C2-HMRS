1c7597dcaf6ea4bba86bde1cdcf418b8938beff7
HazemHalawi <Hazem.halawi@isae.edu.lb>
2018-08-22 12:09:47 +0300

    User can now Login with email and password

--- app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/LoginPage.java
+++ app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/LoginPage.java
@@ -1,7 +1,9 @@
 package com.example.mariobousamra.projet_c2_hmrs;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -19,6 +28,8 @@ public class LoginPage extends AppCompatActivity {
     private TextView SignUp;
     private Button Login;
     private LinearLayout llayout;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +37,32 @@ public class LoginPage extends AppCompatActivity {
         setContentView(R.layout.activity_login_page);
 
 
-        Name = (EditText)findViewById(R.id.etName);
+
+
+        Name = (EditText)findViewById(R.id.etEmail);
         Password = (EditText)findViewById(R.id.etPass);
         ForgotPassword = (TextView)findViewById(R.id.tvForgotPass);
 
         Login = (Button)findViewById(R.id.btnLogin);
         SignUp = (TextView)findViewById(R.id.tvSignUp);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog  =new ProgressDialog(this);
 
+        //Checking if a user is already logged in
+        /*FirebaseUser user = firebaseAuth.getCurrentUser();
+        if(user!=null){
+            //finish this activity
+            finish();
+            //and direct him to the activity
+            startActivity(new Intent(LoginPage.this, MainActivity.class));
+        }*/
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(Name.getText().toString(), Password.getText().toString());
+            }
+        });
 
         SignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +80,25 @@ public class LoginPage extends AppCompatActivity {
         Intent intent = new Intent(LoginPage.this, SignupPage.class);
         startActivity(intent);
         //}
+    }
+
+    private void login(String user_email, String user_password){
+
+        progressDialog.setMessage("Please wait");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(user_email, user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginPage.this,"Login Successful", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginPage.this, MainActivity.class));
+                }else{
+                    Toast.makeText(LoginPage.this,"Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 
