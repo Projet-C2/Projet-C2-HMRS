31a3b028e5306cc1c9852b490941d637ffbd10b3
HazemHalawi <Hazem.halawi@isae.edu.lb>
2018-08-23 10:08:48 +0300

    Added ForgotPassword features

--- app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/ForgotPassword.java
+++ app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/ForgotPassword.java
@@ -2,12 +2,23 @@ package com.example.mariobousamra.projet_c2_hmrs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ForgotPassword extends AppCompatActivity {
+
+    private EditText passwordEmail;
+    private Button resetPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        passwordEmail = (EditText)findViewById(R.id.etPasswordEmail);
+        resetPassword = (Button)findViewById(R.id.btnReset);
+
+
     }
 }
