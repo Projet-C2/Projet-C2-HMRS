6875f95ce61d8df78f83cf129e9ed16a29c368d6
mariobs1990 <mario.bousamra@isae.edu.lb>
2018-08-31 22:08:36 +0300

    add the welcome Name in the vendor page, but need to arrange the database to be able to retrieve the correctly.

--- app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/VendorPage.java
+++ app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/VendorPage.java
@@ -1,16 +1,23 @@
 package com.example.mariobousamra.projet_c2_hmrs;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class VendorPage extends AppCompatActivity {
 
@@ -19,6 +26,9 @@ public class VendorPage extends AppCompatActivity {
     Button btnAddProduct;
     private FirebaseAuth firebaseAuth;
 
+    private DatabaseReference mDatabase;
+    private TextView mNameView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +36,27 @@ public class VendorPage extends AppCompatActivity {
         setContentView(R.layout.activity_vendor_page);
 
         firebaseAuth = FirebaseAuth.getInstance();
+
+        //Show the Name of logged in user, WELCOME USER.
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child("User1").child("Name");
+        mNameView = (TextView) findViewById(R.id.name_view);
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                String name = dataSnapshot.getValue().toString();
+                mNameView.setText(name);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
         Logout = (Button)findViewById(R.id.btnLogout);
         btnAddProduct = (Button) findViewById(R.id.btn_add_product);
 
