2d8c21058641aeac82ecb9ad92236f4a5b5e1a28
HazemHalawi <Hazem.halawi@isae.edu.lb>
2018-09-02 19:16:28 +0300

    Retrieve product name and add it to listview (not by category)

--- app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/HotelsPage.java
+++ app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/HotelsPage.java
@@ -4,6 +4,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,9 +16,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class HotelsPage extends AppCompatActivity {
 
     private TextView Title;
+    private ListView listView;
+    String[] ListElements = new String[] {};
+
 
     private FirebaseDatabase firebaseDatabase;
     private FirebaseAuth firebaseAuth;
@@ -28,6 +37,15 @@ public class HotelsPage extends AppCompatActivity {
 
         Title = (TextView)findViewById(R.id.tvTitle);
 
+
+        listView = (ListView)findViewById(R.id.databaseListview);
+        final List < String > ListElementsArrayList = new ArrayList< String>(Arrays.asList(ListElements));
+        final ArrayAdapter< String > adapter = new ArrayAdapter < String >
+                (HotelsPage.this, android.R.layout.simple_list_item_1, ListElementsArrayList);
+        listView.setAdapter(adapter);
+
+
+
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
         //DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());//"cCyQM76KQ3gaWusydXY1HjkrKFB3");
@@ -36,6 +54,7 @@ public class HotelsPage extends AppCompatActivity {
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //iterating users
                for(DataSnapshot item_snapshot:dataSnapshot.getChildren()) {
 //                    try {
 //                        // Toast.makeText(HotelsPage.this, "item id " + item_snapshot.child("product_category").getRef().addValueEventListener().toString(), Toast.LENGTH_SHORT).show();
@@ -43,18 +62,20 @@ public class HotelsPage extends AppCompatActivity {
 //                    }catch (Exception ex){
 //
 //                    }
+                   //iterating user fields
+                   for(DataSnapshot info_item_snapshot:item_snapshot.getChildren()) {
+                       try {
+                           //UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+                           //Toast.makeText(HotelsPage.this, "" + info_item_snapshot.child("product_name").getValue().toString(), Toast.LENGTH_SHORT).show();
+                           ListElementsArrayList.add(info_item_snapshot.child("product_name").getValue().toString());
+                           adapter.notifyDataSetChanged();
 
-                   try{
-                       //UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
-                       Toast.makeText(HotelsPage.this,""+item_snapshot.child("Name").getValue().toString(), Toast.LENGTH_SHORT).show();
-                   }catch (Exception ex){
-                       Toast.makeText(HotelsPage.this,""+ex, Toast.LENGTH_LONG).show();
+                       } catch (Exception ex) {
+                           //Toast.makeText(HotelsPage.this, "" + ex, Toast.LENGTH_LONG).show();
+                       }
                    }
                }
-
-
-
-
+               Toast.makeText(HotelsPage.this, "Please select a product", Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -62,6 +83,7 @@ public class HotelsPage extends AppCompatActivity {
                 Toast.makeText(HotelsPage.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
             }
         });
+
 
     }
 
