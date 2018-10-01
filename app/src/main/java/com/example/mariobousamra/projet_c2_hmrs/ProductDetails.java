ed8a852ce225d80736a9c4134d6db15eb57f1d43
HazemHalawi <Hazem.halawi@isae.edu.lb>
2018-10-01 11:43:39 +0300

    Get the Uid of the user who uploaded the image

--- app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/ProductDetails.java
+++ app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/ProductDetails.java
@@ -1,6 +1,7 @@
 package com.example.mariobousamra.projet_c2_hmrs;
 
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -9,6 +10,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
@@ -21,12 +27,12 @@ public class ProductDetails extends AppCompatActivity {
     TextView tvdescription;
     TextView tvStatus;
 
-
-
-
     ImageView ivProdImage;
 
     private FirebaseStorage firebaseStorage;
+    private FirebaseDatabase firebaseDatabase;
+
+    public String Uidwithimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,21 +47,60 @@ public class ProductDetails extends AppCompatActivity {
         ivProdImage = findViewById(R.id.ivProdImage);
 
 
-        String product_name = getIntent().getExtras().getString("product_name");
+        final String product_name = getIntent().getExtras().getString("product_name");
 
         //if(product_name != null ){
           //  Toast.makeText(ProductDetails.this, "product_name : " + product_name, Toast.LENGTH_SHORT).show();
         //}
 
 
-        firebaseStorage = FirebaseStorage.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //iterating users
+                outerloop:
+                for(DataSnapshot item_snapshot:dataSnapshot.getChildren()) {
+                    //iterating user fields
+                    for(DataSnapshot info_item_snapshot:item_snapshot.getChildren()) {
+                        try {
+                            String prod_category = info_item_snapshot.child("product_name").getValue().toString();
+                            if(prod_category.equals(product_name)) {
+                                //get the Uid ex: QNqWIXGKySORpTcWpv0HLXV03Ax2
+                                Uidwithimage = item_snapshot.getRef().getKey().toString();
+                                //if(Uidwithimage != null ){
+                                  //Toast.makeText(ProductDetails.this, "Uid : " + Uidwithimage, Toast.LENGTH_LONG).show();
+                                //}
+                                tvName.setText(info_item_snapshot.child("product_name").getValue().toString());
+                                tvPrice.setText(info_item_snapshot.child("product_price").getValue().toString());
+                                tvdescription.setText(info_item_snapshot.child("product_description").getValue().toString());
+                                tvStatus.setText(info_item_snapshot.child("product_status").getValue().toString());
+                                break outerloop;
+                            }
+                        } catch (Exception ex) {
+                            //Toast.makeText(HotelsPage.this, "" + ex, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
 
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ProductDetails.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        firebaseStorage = FirebaseStorage.getInstance();
         StorageReference storageReference = firebaseStorage.getReference();
-/*        storageReference.child().child(product_name).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+/*        storageReference.child(Uidwithimage).child(product_name).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
             public void onSuccess(Uri uri) {
                 Picasso.get().load(uri).fit().centerCrop().into(ivProdImage);
             }
         });*/
+
     }
 }
